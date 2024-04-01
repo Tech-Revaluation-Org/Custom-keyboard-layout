319db35b1e21e5b1e5007809a4b9718311b7e302
Faiaz <48489726+Faiaz3k@users.noreply.github.com>
2024-04-01 07:11:26 +0600

    Create main.rb

---
+++ Linux/main.rb
@@ -0,0 +1,16 @@
+# Define your custom key mappings
+key_mappings = {
+  'a' => 'q',
+  'b' => 'w',
+  # Add more mappings as needed
+}
+
+# Write the custom key mappings to an XKB configuration file
+File.open('/usr/share/X11/xkb/symbols/custom', 'w') do |file|
+  key_mappings.each do |from_key, to_key|
+    file.puts "key <#{from_key.upcase}> { [ #{to_key} ] };"
+  end
+end
+
+# Apply the custom keyboard layout
+system('setxkbmap custom')
